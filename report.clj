4552d62606a01f74e10e44d255b5185a2455e288
TJ <thaddeusjiang@gmail.com>
2022-12-31 16:54:39 +0900

    top 10 directories

--- report.clj
+++ report.clj
@@ -1,22 +1,60 @@
 #!/usr/bin/env bb
 
+(require '[clojure.string :as string])
 (def contents (slurp "history.txt"))
 
-(defn parse-line [line]
+(defn- parse-line [line]
   (let [rest (subs line 7)]
-    (first (clojure.string/split rest #"\s+"))))
+    (first (string/split rest #"\s+"))))
 
+(def commands (string/split-lines contents))
 
-(def commands (map parse-line (clojure.string/split contents #"\n")))
-(def data (group-by identity commands))
-;; get data count
-(def data-count (map (fn [[k v]] [k (count v)]) data))
-;; sort by count desc
-(def data-count (sort-by second data-count))
-;; get top 10
-(def data-count (take 10 (reverse data-count)))
-;; print
-(println "--top 10 commands--\n")
-(doseq [[k v] data-count]
-  (println k v))
-(println "\n--total--" (count commands))
+(->> commands
+     (count)
+     (def total))
+
+(->> commands
+     (map parse-line)
+     (group-by identity)
+     (map (fn [[k v]] [k (count v)]))
+     (sort-by second)
+     (reverse)
+     (take 10)
+     (def top-commands))
+
+;; top 10 directories
+
+(defn- ignore-number [line]
+  (subs line 7))
+
+(defn- get-directory [line]
+  (second (string/split line #"\s+")))
+
+(->> commands
+     (map ignore-number)
+     (filter #(string/starts-with? % "cd"))
+     (map get-directory)
+     (filter #(not (string/blank? %)))
+     (filter #(not (string/starts-with? % "..")))
+     (group-by identity)
+     (map (fn [[k v]] [k (count v)]))
+     (sort-by second)
+     (reverse)
+     (take 10)
+     (def top-directories))
+
+;; print total
+(println (format "Total: %d" total))
+(println)
+
+;; ;; print top 10 commands
+(println "🏆Top 10 commands")
+(doseq [[value count] top-commands]
+  (println (format "%6d %s" count value)))
+(println)
+
+;; print top 10 directories
+(println "📂Top 10 directories")
+(doseq [[value, count] top-directories]
+  (println (format "%6d %s" count value)))
+(println)

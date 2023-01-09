82596f29ef97f85acf9a3d3bbbd8800233eba7d2
TJ <thaddeusjiang@gmail.com>
2023-01-09 20:26:11 +0900

    wip: bb.edn :paths and bbin/bin

--- src/chr/chr.clj
+++ src/chr/chr.clj
@@ -33,99 +33,100 @@
 (defn- get-directory [line]
   (second (string/split line #"\s+")))
 
+(defn -main [& _]
 ;; total number of commands
-(->> commands
-     (count)
-     (def total))
-(println (format "Total: %d" total))
-(println)
+  (->> commands
+       (count)
+       (def total))
+  (println (format "Total: %d" total))
+  (println)
 
-(->> commands
-     (keep parse-commands)
-     (group-by identity)
-     (map (fn [[k v]] [k (count v)]))
-     (sort-by second)
-     (reverse)
-     (take 10)
-     (def top-commands))
+  (->> commands
+       (keep parse-commands)
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (sort-by second)
+       (reverse)
+       (take 10)
+       (def top-commands))
 
-(println "🏆Top 10 commands")
-(doseq [[value count] top-commands]
-  (println (format "%6d %s" count value)))
-(println)
+  (println "🏆Top 10 commands")
+  (doseq [[value count] top-commands]
+    (println (format "%6d %s" count value)))
+  (println)
 
 ;; top 10 directories
-(->> commands
-     (map ignore-timestamp-str)
-     (filter #(string/starts-with? % "cd"))
-     (map get-directory)
-     (filter #(not (string/blank? %)))
-     (filter #(not (string/starts-with? % "..")))
-     (group-by identity)
-     (map (fn [[k v]] [k (count v)]))
-     (sort-by second)
-     (reverse)
-     (take 10)
-     (def top-directories))
+  (->> commands
+       (map ignore-timestamp-str)
+       (filter #(string/starts-with? % "cd"))
+       (map get-directory)
+       (filter #(not (string/blank? %)))
+       (filter #(not (string/starts-with? % "..")))
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (sort-by second)
+       (reverse)
+       (take 10)
+       (def top-directories))
 
-(println "📂Top 10 directories")
-(doseq [[value, count] top-directories]
-  (println (format "%6d %s" count value)))
-(println)
+  (println "📂Top 10 directories")
+  (doseq [[value, count] top-directories]
+    (println (format "%6d %s" count value)))
+  (println)
 
-(defn- pickup-datetime-str [line]
-  (subs line 2 12))
+  (defn- pickup-datetime-str [line]
+    (subs line 2 12))
 
 ;; the busiest day
-(->> commands
-     (map pickup-datetime-str)
-     (map parse-date)
-     (map (fn [date] (format "%tF" date)))
-     (group-by identity)
-     (map (fn [[k v]] [k (count v)]))
-     (sort-by second)
-     (reverse)
-     (take 1)
-     (def busiest-day))
+  (->> commands
+       (map pickup-datetime-str)
+       (map parse-date)
+       (map (fn [date] (format "%tF" date)))
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (sort-by second)
+       (reverse)
+       (take 1)
+       (def busiest-day))
 
-(println "💦The busiest day")
-(doseq [[value, count] busiest-day]
-  (println (format "%6d commands on %s" count value)))
-(println)
+  (println "💦The busiest day")
+  (doseq [[value, count] busiest-day]
+    (println (format "%6d commands on %s" count value)))
+  (println)
 
-(def weekday-english ["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"])
-(defn translate-weekday [index]
-  (get weekday-english index))
+  (def weekday-english ["Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday"])
+  (defn translate-weekday [index]
+    (get weekday-english index))
 
-(defn weekday [date-string]
-  (.getDay (parse-date date-string)))
+  (defn weekday [date-string]
+    (.getDay (parse-date date-string)))
 
 ;; Weekly Activity
-(->> commands
-     (map pickup-datetime-str)
-     (map weekday)
-     (group-by identity)
-     (map (fn [[k v]] [k (count v)]))
-     (sort-by first)
-     (map (fn [[k v]] [(translate-weekday k) v]))
-     (def weekly-usage))
+  (->> commands
+       (map pickup-datetime-str)
+       (map weekday)
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (sort-by first)
+       (map (fn [[k v]] [(translate-weekday k) v]))
+       (def weekly-usage))
 
-(println "📅Weekly Activity")
-(doseq [[value, count] weekly-usage]
-  (println (format "%10s %s" value (string/join (repeat (/ count 100) "█")))))
-(println)
+  (println "📅Weekly Activity")
+  (doseq [[value, count] weekly-usage]
+    (println (format "%10s %s" value (string/join (repeat (/ count 100) "█")))))
+  (println)
 
 ;; Daily Activity
-(->> commands
-     (map pickup-datetime-str)
-     (map parse-date)
-     (map (fn [date] (format "%tH" date)))
-     (group-by identity)
-     (map (fn [[k v]] [k (count v)]))
-     (sort-by first)
-     (def hourly-usage))
+  (->> commands
+       (map pickup-datetime-str)
+       (map parse-date)
+       (map (fn [date] (format "%tH" date)))
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))
+       (sort-by first)
+       (def hourly-usage))
 
-(println "🕙Daily Activity")
-(doseq [[value, count] hourly-usage]
-  (println (format "%6s:00 %s" value (string/join (repeat (/ count 100) "█")))))
-(println)
+  (println "🕙Daily Activity")
+  (doseq [[value, count] hourly-usage]
+    (println (format "%6s:00 %s" value (string/join (repeat (/ count 100) "█")))))
+  (println))
